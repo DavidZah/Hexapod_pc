9732a4d5bc2f3545f07af24add9e96b5ab7ac59b
DavidZah <davidzah6@seznam.cz>
2019-02-04 16:00:44 +0100

    Add start of learnig

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
+max guess 300
+
+g = 9.780
 def result (angle):
-    
+    if(angle < 0):
+        angle = 0
+    if(angle > 180):
+        angle = 180
+    if(angle <= 90):
+        multipler = g/90
+        return angle * multipler
+    else:
+        multipler = g/90
+        angle = angle - 90
+        return -(angle*multipler)
+
+def get_score(data,expectet_data):
+    score = result(data)
+    score_percent = (abs(score)/expectet_data)*100
+    return score_percent
+
+
